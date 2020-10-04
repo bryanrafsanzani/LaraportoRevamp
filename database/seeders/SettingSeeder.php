8da702ca3652fac85f891119e9837bc1eda7902d
Bryan Rafsanzani <bryanrafsanzani46@gmail.com>
2020-10-04 21:02:55 +0700

    add public vendor data

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -13,6 +13,22 @@ class SettingSeeder extends Seeder
      */
     public function run()
     {
-        //
+           /**
+             * Setting need
+             * app_name
+             * site_title
+             * icon_site
+             *  footer_content_cms
+             * footer_content_public
+             */
+        $settings = [
+            [
+                'name'          =>  '',
+                'option_name'   =>  '',
+                'value'         =>  '',
+                'default_value' =>  '',
+                'description'   =>  '',
+            ]
+        ];
     }
 }
