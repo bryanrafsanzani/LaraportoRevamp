c92e81b18b793da64c564b7461d02e95286fa2cb
Bryan Rafsanzani <bryanrafsanzani46@gmail.com>
2020-11-01 23:54:42 +0700

    update route api

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,6 @@ use App\Http\Controllers\Api\AuthController,
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
 
 Route::prefix('v1')->group(function () {
 
