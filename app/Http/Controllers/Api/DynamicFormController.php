3970fe9c8d3fb0a8a4839e2a80d2c51198f91a9d
Bryan Rafsanzani <bryanrafsanzani46@gmail.com>
2020-11-13 21:52:44 +0700

    api store dynamicForm

--- app/Http/Controllers/Api/DynamicFormController.php
+++ app/Http/Controllers/Api/DynamicFormController.php
@@ -23,4 +23,29 @@ class DynamicFormController extends Controller
             "data"      =>  $dynamicForms
         ], \HttpStatus::OK);
     }
+
+    public function store(Request $request)
+    {
+        $rules     = [
+            'name'         => 'required|string|max:181',
+            'data_type'    => 'required|in:integer,double,text,long_text,boolean,date,0',
+            'parent'       => 'required|number|max:20',
+            'required'     => 'required|in:0,1',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return  \MessageHelper::unprocessableEntity($validator->messages());
+        }
+        $dynamicForm = DynamicForm::create($request->all());
+
+        return response()->json([
+            "code"      =>  \HttpStatus::OK,
+            "status"    =>  true,
+            "message"   =>  "Success, data Dynamic Form was inserted!",
+            "data"      =>  null
+        ], \HttpStatus::OK);
+
+    }
 }
