96243b4b515a5326d682f1c2f8e8dc337717f16a
Bryan Rafsanzani <bryanrafsanzani46@gmail.com>
2020-11-13 21:09:12 +0700

    add setting index api

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Setting;
 
 class SettingController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('jwt.verify');
+    }
+
+    public function index()
+    {
+        $settings = Setting::all();
+        return response()->json([
+            "code"      =>  \HttpStatus::OK,
+            "status"    =>  true,
+            "message"   =>  "Success, " .$settings->count(). " data settings found",
+            "data"      =>  $settings
+        ], \HttpStatus::OK);
+    }
 }
