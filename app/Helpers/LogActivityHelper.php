d31ca8ce7e1e4a137e38f2e3e3d516964750a06f
Bryan Rafsanzani <bryanrafsanzani46@gmail.com>
2021-01-17 23:53:34 +0700

    add baseLogActivity

--- app/Helpers/LogActivityHelper.php
+++ app/Helpers/LogActivityHelper.php
@@ -1,9 +1,21 @@
 <?php
 namespace App\Helpers;
-
+use Illuminate\Support\Facades\Route;
+use Request;
 
 class LogActivityHelper
 {
+    public static function baseLog($msg)
+    {
+        $array['user_id']      = is_null(auth()->user()) ? 0 : auth()->user()->id;
+        $array['ip']           = Request::ip();
+        $array['page']         = Request::url();
+        $array['method']       = Route::getCurrentRoute()->methods[0];
+        $array['route_name']   = Route::currentRouteName();
+        $array['data']         = $msg;
+        $array['access_date']  = now();
+        return $array;
+    }
 
 
 }
